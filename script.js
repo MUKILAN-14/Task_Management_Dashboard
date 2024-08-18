11dd2683453b74100a51f23580e1022f98b91044
Mukilan <mukilan671@gmail.com>
2024-08-18 18:48:27 +0530

    ch

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ function navigateTodashboard(){
     window.location.href='index2.html';
 }
 function addingtasks(){
-    window.location.href='index.html';
-}
+    const cont=document.querySelector(".box")
+    let newele=document.createElement("div")
+    newele.innerHTML="<p>Using either of these methods will ensure that your div elements wrap correctly, preventing them from overflowing off the screen.</p>"
+    cont.prepend(newele)
+  }
